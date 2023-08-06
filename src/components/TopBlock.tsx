83e4d312a8eb6cb9db3bbd55ad6fb2363cc95c44
atryknaav <aetryknaavzaaroyt@gmail.com>
2023-08-06 13:15:41 -0700

    Fixed top block's animation

--- src/components/TopBlock.tsx
+++ src/components/TopBlock.tsx
@@ -18,29 +18,29 @@ const TopBlock = (props: { name: string, status: string, weekPercent: string, we
 
   return (
     
-    <div className={'flex flex-col flex-1 first:rounded-l-lg last:rounded-r-lg p-3 mt-20 lg:mt-0 border-r-[1px] border-border last:border-none' + styleBody}>
+    <div className={'flex flex-col group flex-1 first:rounded-l-lg last:rounded-r-lg p-3 mt-20 lg:mt-0 border-r-[1px] border-border last:border-none' + styleBody}>
 
         <div className="flex gap-2">
-            <div className={`rounded-full p-1 h-[fit-content] bg-black ${name === 'Total sales' ? 'bg-black text-success' : 'dark:bg-light-gray bg-black hover:bg-black hover:text-success'}`}>
+            <div className={`rounded-full p-1 h-[fit-content] bg-black ${name === 'Total sales' ? 'bg-black text-success' : 'dark:bg-light-gray bg-black group-hover:bg-black group-hover:text-success'}`}>
                 {icon}
             </div>
 
-            <div className='text-md mb-6'>
+            <div className={`text-md mb-6 ${name === 'Total sales' ? 'text-black' : 'text-white group-hover:text-black'}`}>
                 {name}
             </div>
         </div>
 
-        <div className='mb-3 font-semibold text-3xl'>
+        <div className={`mb-3 font-semibold text-3xl ${name === 'Total sales' ? 'text-black' : 'text-white group-hover:text-black'}`}>
             {`${status}`}
         </div>
 
         <div className='flex gap-3 text-sm font-medium mb-1'>
 
-            <div className={`flex items-center gap-2 ${name === 'Total sales' ? 'text-minor-success' : 'text-success'}`}>
+            <div className={`flex items-center gap-2 ${name === 'Total sales' ? 'text-minor-success' : 'text-success group-hover:text-minor-success'}`}>
                 {arrowUp} {`${weekPercent}%`}
             </div>
 
-            <div className={`text-minor-dark ${name === 'Total sales' ? 'text-minor-success' : 'text-minor-dark'}`}>
+            <div className={`text-minor-dark ${name === 'Total sales' ? 'text-minor-success' : 'text-minor-dark group-hover:text-minor-success'}`}>
                 {`+${weekNum} this week`}
             </div>
 
